9d8103ebb82441cd10f623667bee9f4a54a84cb9
roberto <roberto.dessi11@gmail.com>
2017-12-07 14:23:00 +0100

    improved cmdline parser, not completed yet

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ import argparse
 
 python main.py
 
+* won't be implemented in alpha version
+
+[-s] [--remove stopwords]
+
 [-k] [--keywords]
 
     [window = between 2 and 10]
 
-
+    * -sim = k [add link if cos similarity between two words is >= k
 [-s] [--summary]
 
     [-l] [--lemmatize]
 
     [-p] [--pos]
 
-    [similarity = common_words | cosine ...]
+    * [similarity = common_words | cosine ...]
 
-    [-v] [--visualize]
+    * [-v] [--visualize]
 
-    [ratio = integer]
+    * [ratio = integer]
 
 pagerank visualization python library: https://graph-tool.skewed.de/static/doc/centrality.html#graph_tool.centrality.pagerank
 
@@ -31,21 +35,44 @@ http://www.graphviz.org/gallery/
 """
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Run automatic keywords extraction or extractive summerizer')
+def check_args(opt):
+    return True
 
-    parser.add_argument('-k', '--keywords')
+def parse_cmdline():
+    parser = argparse.ArgumentParser(description='Run automatic keywords extraction or extractive summarizer')
 
-    parser.add_argument('-s', '--summary')
+    group = parser.add_mutually_exclusive_group()
 
-    parser.add_argument('-l', '--lemmatize')
+    group.add_argument('--keywords', action = 'store_true', default = False,
+                        help = 'preforming kwywords extraction task')
 
-    parser.add_argument('-p', '--pos')
+    group.add_argument('--summary', action = 'store_true', default = False,
+                        help = 'performing sentence extraction/summarization task')
 
-    #parser.add_argument('-v', '--visualize')
+    parser.add_argument('-f', '--file',  type=argparse.FileType('r'))
 
-    parser.print_help()
+    parse.add_argument('-n', '--no_stopwords', action = 'store_true',
+                        default = False, helpl = 'removing stopwords from input text')
+
+    parse_add_argument('-w', '--window', default = 2, help = 'window size to create' +
+                        'to create link between keywords')
+
+
+    parser.add_argument('-l', 'lemmatize', action = 'store_true', default = False,
+                        help= 'lemmatizing input text')
+
+    parser.add_argument('-p', '--pos', action = 'store_true', default = False,
+                        help = 'pos tagging input text')
+
+
+
     print('hello')
+    parser.print_help()
+    print('done')
+
+def main():
+    print('hello')
+
 
 if __name__ == "__main__":
     main()
